1102c7f55b89687c23e64966f0a72885fbf79247
jinjadarkwebjinja <88076807+jinjadarkwebjinja@users.noreply.github.com>
2021-09-27 15:12:34 +0530

    Update sampletest.groovy

--- vars/sampletest.groovy
+++ vars/sampletest.groovy
@@ -5,8 +5,13 @@ def call() {
                 sh 'npm install'
                 echo 'Build is done guys!'
 }
+def call ()
 {
-   Date date = new Date()
-   return date.format('yyyyMMddHHmmss',TimeZone.getTimeZone('PST')) as String
+ echo "The build number is ${env.BUILD_NUMBER}"
+                echo "The job name is ${env.JOB_NAME}"
+                echo "The Jenkins URL is ${env.JENKINS_URL}"
+                echo "The Build URL is ${env.BUILD_URL}"
+                echo "The GIt Branch URL is ${env.GIT_BRANCH}"
+                echo "The Jenkins Veedu URL is ${env.JENKINS_HOME}"
+                echo "The Jenkins Workspace is ${env.WORKSPACE}"
 }
-return this
