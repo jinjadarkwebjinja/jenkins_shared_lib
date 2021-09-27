e8ccd5a3a700d59c435d02803dcc92f6ead61a71
jinjadarkwebjinja <88076807+jinjadarkwebjinja@users.noreply.github.com>
2021-09-27 15:09:38 +0530

    Update sampletest.groovy

--- vars/sampletest.groovy
+++ vars/sampletest.groovy
@@ -5,4 +5,8 @@ def call() {
                 sh 'npm install'
                 echo 'Build is done guys!'
 }
-
+{
+   Date date = new Date()
+   return date.format('yyyyMMddHHmmss',TimeZone.getTimeZone('PST')) as String
+}
+return this
