df47c22e201d7948ad718bcdfb5656ef42ced88f
jinjadarkwebjinja <88076807+jinjadarkwebjinja@users.noreply.github.com>
2021-09-27 15:04:36 +0530

    Update sampletest.groovy

--- vars/sampletest.groovy
+++ vars/sampletest.groovy
@@ -1,3 +1,11 @@
+def String call() {
+  def commit = null
+  def changeLogSets = currentBuild.changeSets
+  if(changeLogSets.size()>0 && changeLogSets.items.size() > 0){
+    commit = changeLogSets[0].items[0]
+  }
+  return commit
+}
 def call() {
  
                 echo "Build the app"
