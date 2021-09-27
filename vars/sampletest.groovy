64a7e2fb42b0bca253c3462a38eac4713b41dd68
jinjadarkwebjinja <88076807+jinjadarkwebjinja@users.noreply.github.com>
2021-09-27 14:52:54 +0530

    Update sampletest.groovy

--- vars/sampletest.groovy
+++ vars/sampletest.groovy
@@ -1,10 +1,14 @@
 def call() {
+ stage ("Build"){
+                steps{
  
                 echo "Build the app"
                 sh 'npm install'
                 echo 'Build is done guys!'
 }
-{
+ }
+  stage ("Env Build Number"){
+            steps{
                 echo "The build number is ${env.BUILD_NUMBER}"
                 echo "The job name is ${env.JOB_NAME}"
                 echo "The Jenkins URL is ${env.JENKINS_URL}"
@@ -13,7 +17,11 @@ def call() {
                 echo "The Jenkins Veedu URL is ${env.JENKINS_HOME}"
                 echo "The Jenkins Workspace is ${env.WORKSPACE}"
             }
-{
+  }
+  stage('Build Status Through Email Notification'){
+            steps{
             mail bcc: 'sridhar.vimala@infosys.com', body: ''' Thanks,
 Sangi Mangi & Mangi Sangi''', cc: 'jinjajenkinsuser@gmail.com', from: '', replyTo: '', subject: "${env.JOB_NAME}" , to: ''
+}
+  }
 }
